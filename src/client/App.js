f4badbaa6be11430ba3d80b797f80909a07822c0
rmaheshwari <rmaheshwari4@sapient.com>
2019-04-02 22:49:09 +0100

    auth0 changes

--- src/client/App.js
+++ src/client/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Navbar from './Navbar';
 import './app.css';
 import ReactImage from './react.png';
 
@@ -16,7 +17,7 @@ export default class App extends Component {
     return (
       <div>
         {username ? <h1>{`Hello ${username}`}</h1> : <h1>Loading.. please wait!</h1>}
-        <img src={ReactImage} alt="react" />
+        <Navbar />
       </div>
     );
   }
---
+++ src/client/Navbar.js
@@ -0,0 +1,27 @@
+import React from 'react';
+import auth0Client from './Auth';
+
+function NavBar(props) {
+  const signOut = () => {
+    auth0Client.signOut();
+  };
+
+  return (
+    <nav className="navbar navbar-dark bg-primary fixed-top">
+      {
+        !auth0Client.isAuthenticated() &&
+        <p className="btn btn-dark" onClick={auth0Client.signIn}>Sign In</p>
+      }
+      {
+        auth0Client.isAuthenticated() && (
+        <div>
+          <span className="mr-2 text-white">{auth0Client.getProfile().name}</span>
+          <div className="btn btn-dark" onClick={() => {signOut()}}>Sign Out</div>
+        </div>
+        )
+      }
+    </nav>
+  );
+}
+auth0Client.signIn();
+export default NavBar;
